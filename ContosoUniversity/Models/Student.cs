a752e358d333353fc582ea9a92908d236e2c4d17
louie1966 <renedeleeuw66@gmail.com>
2017-09-10 20:30:32 +0200

    The Column Attribute

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ContosoUniversity.Models {
     public class Student {
         public int ID { get; set; }
@@ -8,7 +10,7 @@ namespace ContosoUniversity.Models {
         [StringLength(50)]
         public string LastName { get; set; }
 
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters."), Column("FirstName")]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
