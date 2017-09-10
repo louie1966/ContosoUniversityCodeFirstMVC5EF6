c7c50c6da75184a8a5325eac52d33e95adabb7b0
louie1966 <renedeleeuw66@gmail.com>
2017-09-10 11:15:25 +0200

    Add Sorting Functionality to the Index Method

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -14,8 +14,26 @@ namespace ContosoUniversity.Controllers {
         private SchoolContext db = new SchoolContext();
 
         // GET: Student
-        public ActionResult Index() {
-            return View(db.Students.ToList());
+        public ActionResult Index(string sortOrder) {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            var students = from s in db.Students
+                           select s;
+            switch (sortOrder) {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+            return View(students.ToList());
         }
 
         // GET: Student/Details/5
