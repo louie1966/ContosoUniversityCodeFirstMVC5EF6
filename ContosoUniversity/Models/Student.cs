bd402e492cfc7eae14eb905d24552c3daa682943
louie1966 <renedeleeuw66@gmail.com>
2017-09-10 20:13:35 +0200

    The DataType Attribute

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models {
     public class Student {
-            public int ID { get; set; }
-            public string LastName { get; set; }
-            public string FirstMidName { get; set; }
-            public DateTime EnrollmentDate { get; set; }
-            public virtual ICollection<Enrollment> Enrollments { get; set; }
-        }
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
+}
