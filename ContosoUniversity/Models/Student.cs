9c1a829629e578d764d18db7f2fbc764388c45b1
louie1966 <renedeleeuw66@gmail.com>
2017-09-10 20:40:22 +0200

    Complete Changes to the Student Entity

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -7,14 +7,23 @@ namespace ContosoUniversity.Models {
     public class Student {
         public int ID { get; set; }
 
-        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
 
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters."), Column("FirstName")]
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
-        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName { get { return LastName + ", " + FirstMidName; } }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
