f92c44fd2491aa4f658d5914e011080f70325de5
louie1966 <renedeleeuw66@gmail.com>
2017-09-11 14:26:04 +0200

    Modify the Department Controller

--- ContosoUniversity/Controllers/DepartmentController.cs
+++ ContosoUniversity/Controllers/DepartmentController.cs
@@ -1,47 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
-using ContosoUniversity.DAL;
+﻿using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
-namespace ContosoUniversity.Controllers
-{
-    public class DepartmentController : Controller
-    {
+namespace ContosoUniversity.Controllers {
+    public class DepartmentController : Controller {
         private SchoolContext db = new SchoolContext();
 
         // GET: Department
-        public async Task<ActionResult> Index()
-        {
+        public async Task<ActionResult> Index() {
             var departments = db.Departments.Include(d => d.Administrator);
             return View(await departments.ToListAsync());
         }
 
         // GET: Department/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<ActionResult> Details(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Department department = await db.Departments.FindAsync(id);
-            if (department == null)
-            {
+            if (department == null) {
                 return HttpNotFound();
             }
             return View(department);
         }
 
         // GET: Department/Create
-        public ActionResult Create()
-        {
-            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName");
+        public ActionResult Create() {
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "FullName");
             return View();
         }
 
@@ -50,32 +41,27 @@ namespace ContosoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "DepartmentID,Name,Budget,StartDate,InstructorID")] Department department)
-        {
-            if (ModelState.IsValid)
-            {
+        public async Task<ActionResult> Create([Bind(Include = "DepartmentID,Name,Budget,StartDate,InstructorID")] Department department) {
+            if (ModelState.IsValid) {
                 db.Departments.Add(department);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName", department.InstructorID);
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "FullName", department.InstructorID);
             return View(department);
         }
 
         // GET: Department/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<ActionResult> Edit(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Department department = await db.Departments.FindAsync(id);
-            if (department == null)
-            {
+            if (department == null) {
                 return HttpNotFound();
             }
-            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName", department.InstructorID);
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "FullName", department.InstructorID);
             return View(department);
         }
 
@@ -84,28 +70,62 @@ namespace ContosoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "DepartmentID,Name,Budget,StartDate,InstructorID")] Department department)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(department).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+        public async Task<ActionResult> Edit(
+ [Bind(Include = "DepartmentID, Name, Budget, StartDate, RowVersion, InstructorID")]
+Department department) {
+            try {
+                if (ModelState.IsValid) {
+                    db.Entry(department).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
             }
-            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName", department.InstructorID);
+            catch (DbUpdateConcurrencyException ex) {
+                var entry = ex.Entries.Single();
+                var clientValues = (Department)entry.Entity;
+                var databaseEntry = entry.GetDatabaseValues();
+
+                if (databaseEntry == null) {
+                    ModelState.AddModelError(string.Empty,
+                    "Unable to save changes. The department was deleted by another user.");
+                }
+                else {
+                    var databaseValues = (Department)databaseEntry.ToObject();
+                    if (databaseValues.Name != clientValues.Name)
+                        ModelState.AddModelError("Name", "Current value: "
+                        + databaseValues.Name);
+                    if (databaseValues.Budget != clientValues.Budget)
+                        ModelState.AddModelError("Budget", "Current value: "
+                        + String.Format("{0:c}", databaseValues.Budget));
+                    if (databaseValues.StartDate != clientValues.StartDate)
+                        ModelState.AddModelError("StartDate", "Current value: "
+                        + String.Format("{0:d}", databaseValues.StartDate));
+                    if (databaseValues.InstructorID != clientValues.InstructorID)
+                        ModelState.AddModelError("InstructorID", "Current value: "
+                        + db.Instructors.Find(databaseValues.InstructorID).FullName);
+                    ModelState.AddModelError(string.Empty, "The record you attempted to edit "
+                    + "was modified by another user after you got the original value. The "
+                    + "edit operation was canceled and the current values in the database "
+                    + "have been displayed. If you still want to edit this record, click "
+                    + "the Save button again. Otherwise click the Back to List hyperlink.");
+                    department.RowVersion = databaseValues.RowVersion;
+                }
+            }
+            catch (RetryLimitExceededException /* dex */) {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError(string.Empty, "Unable to save changes. Try again, and if the problem persists contact your system administrator.");
+            }
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "FullName", department.InstructorID);
             return View(department);
         }
 
         // GET: Department/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<ActionResult> Delete(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Department department = await db.Departments.FindAsync(id);
-            if (department == null)
-            {
+            if (department == null) {
                 return HttpNotFound();
             }
             return View(department);
@@ -114,18 +134,15 @@ namespace ContosoUniversity.Controllers
         // POST: Department/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
+        public async Task<ActionResult> DeleteConfirmed(int id) {
             Department department = await db.Departments.FindAsync(id);
             db.Departments.Remove(department);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
                 db.Dispose();
             }
             base.Dispose(disposing);
