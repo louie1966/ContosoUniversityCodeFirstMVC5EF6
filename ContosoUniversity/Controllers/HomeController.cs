4c6aceb37bfe30648d48a1800747ad9614006207
louie1966 <renedeleeuw66@gmail.com>
2017-09-11 15:23:57 +0200

    Calling a Query that Returns Other Types of Objects

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ContosoUniversity.DAL;
 using ContosoUniversity.ViewModels;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -12,12 +13,18 @@ namespace ContosoUniversity.Controllers {
         }
 
         public ActionResult About() {
-            IQueryable<EnrollmentDateGroup> data = from student in db.Students
-                                                   group student by student.EnrollmentDate into dateGroup
-                                                   select new EnrollmentDateGroup() {
-                                                       EnrollmentDate = dateGroup.Key,
-                                                       StudentCount = dateGroup.Count()
-                                                   };
+            //IQueryable<EnrollmentDateGroup> data = from student in db.Students
+            //                                       group student by student.EnrollmentDate into dateGroup
+            //                                       select new EnrollmentDateGroup() {
+            //                                           EnrollmentDate = dateGroup.Key,
+            //                                           StudentCount = dateGroup.Count()
+            //                                       };
+
+            // SQL version of the above LINQ code. 
+
+            string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount " + "FROM Person " + "WHERE Discriminator = 'Student' " + "GROUP BY EnrollmentDate";
+            IEnumerable<EnrollmentDateGroup> data = db.Database.SqlQuery<EnrollmentDateGroup>(query);
+
             return View(data.ToList());
         }
 
